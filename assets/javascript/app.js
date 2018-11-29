c3d24d0442927e86c57e76a5d1c8aabc15061f33
jcasas5253 <skedabble@gmail.com>
2018-11-29 13:43:04 -0800

    added on click function for start-btn and added a start game function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,34 @@
+$(document).ready(function() {
+    //Trivia Questions and Answers
 var questions = [{
-    question: "question holder?",
-    answers: ["a", "b", "c", "d"],
+    question1: "question holder?",
+    answer1: ["a", "b", "c", "d"],
     correct: "b",
 },
 {
-    question: "question holder?",
-    answers: ["a", "b", "c", "d"],
+    question2: "question holder?",
+    answer2: ["a", "b", "c", "d"],
     correct: "a",
 },
 {
-    question: "question holder?",
-    answers: ["a", "b", "c", "d"],
+    question3: "question holder?",
+    answer3: ["a", "b", "c", "d"],
     correct: "d",
 }
 ]
+
 console.log(questions);
+
+
+function startGame(){
+    $("#game").append ("<div>" + questions.question1 + questions.answer1 + "</div>")
+
+}
+$('#start-btn').on('click', function(){
+    startGame();
+});
+});
+
+
+
+
