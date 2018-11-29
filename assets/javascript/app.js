d94d917b545ab8dd30a1855dd3bfa26ff6440de6
jcasas5253 <skedabble@gmail.com>
2018-11-28 16:32:09 -0800

    linked app.js to html; added holder questions/answers to JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var questions = [{
+    question: "question holder?",
+    answers: ["a", "b", "c", "d"],
+    correct: "b",
+},
+{
+    question: "question holder?",
+    answers: ["a", "b", "c", "d"],
+    correct: "a",
+},
+{
+    question: "question holder?",
+    answers: ["a", "b", "c", "d"],
+    correct: "d",
+}
+]
+console.log(questions);
