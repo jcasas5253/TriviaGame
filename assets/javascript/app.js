5b7dadc96ff87c3409fa96c1a716015847d3a17e
jcasas5253 <skedabble@gmail.com>
2018-11-29 16:16:59 -0800

    added more questions and possible answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,34 @@
 $(document).ready(function() {
 
-    //Trivia Questions and Answers
-var question1 = "Question Placeholder?"
-
-
-console.log(question1);
-
+//Hides the answers before start button is clicked
 document.getElementById("answers1").style.visibility = "hidden";
+document.getElementById("answers2").style.visibility = "hidden";
+document.getElementById("answers3").style.visibility = "hidden";
+document.getElementById("answers4").style.visibility = "hidden";
+document.getElementById("answers5").style.visibility = "hidden";
+document.getElementById("question1").style.visibility = "hidden";
+document.getElementById("question2").style.visibility = "hidden";
+document.getElementById("question3").style.visibility = "hidden";
+document.getElementById("question4").style.visibility = "hidden";
+document.getElementById("question5").style.visibility = "hidden";
 
-//Start Button appends the Game and Starts game
+//Start Button appends the Game/Starts game
 function startGame(){
-    $("#question1").append ("<div>" + question1 + "</div>")
-    document.getElementById("answers1").style.visibility = "visible";
-    
-
+document.getElementById("answers1").style.visibility = "visible";
+document.getElementById("answers2").style.visibility = "visible";
+document.getElementById("answers3").style.visibility = "visible";
+document.getElementById("answers4").style.visibility = "visible";
+document.getElementById("answers5").style.visibility = "visible";
+document.getElementById("question1").style.visibility = "visible";
+document.getElementById("question2").style.visibility = "visible";
+document.getElementById("question3").style.visibility = "visible";
+document.getElementById("question4").style.visibility = "visible";
+document.getElementById("question5").style.visibility = "visible";
 }
+
 $('#start-btn').on('click', function(){
     startGame();
     $('#start-btn').hide();
-
 });
 });
 
